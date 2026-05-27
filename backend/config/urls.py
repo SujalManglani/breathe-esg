023ca525df797213b7f7f73b37a4f257dd94db61
chimp-20 <chimpuser281@gmail.com>
2026-05-27 21:10:41 +0530

    changes for the permission of url path

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 
+def home(request):
+    return JsonResponse({
+        "status": "Backend running",
+        "message": "Use /api/ endpoints"
+    })
+
+
 urlpatterns = [
+    path('', home),  # ✅ ADD THIS LINE (IMPORTANT)
+
     path('admin/', admin.site.urls),
 
     path('api/', include('ingestion.urls')),
