83234c670d165dfc952b536f148728ac6fc0831f
chimp-20 <chimpuser281@gmail.com>
2026-05-27 21:35:57 +0530

    change of url section

--- backend/ingestion/urls.py
+++ backend/ingestion/urls.py
@@ -1,15 +1,16 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.http import JsonResponse
-
-def home(request):
-    return JsonResponse({
-        "status": "Backend running",
-        "message": "Use /api/ endpoints"
-    })
+from django.urls import path
+from .views import (
+    get_records,
+    upload_csv,
+    upload_page,
+    dashboard_summary,
+    update_record_status
+)
 
 urlpatterns = [
-    path("", home),
-    path("admin/", admin.site.urls),
-    path("api/", include("ingestion.urls")),
+    path("records/", get_records),
+    path("upload/", upload_csv),
+    path("upload-page/", upload_page),
+    path("summary/", dashboard_summary),
+    path("record/<int:record_id>/status/", update_record_status),
 ]
