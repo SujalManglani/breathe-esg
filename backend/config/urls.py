8e886a3e450beff90092f8a5db966f9ddc7c9d52
chimp-20 <chimpuser281@gmail.com>
2026-05-28 11:51:46 +0530

    changes in urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,28 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
 
-from django.conf import settings
-from django.conf.urls.static import static
-
-
 def home(request):
     return JsonResponse({
         "status": "Backend running",
         "message": "Use /api/ endpoints"
     })
 
-
 urlpatterns = [
-    path('', home),  # ✅ ADD THIS LINE (IMPORTANT)
-
-    path('admin/', admin.site.urls),
-
-    path('api/', include('ingestion.urls')),
+    path("", home),
+    path("admin/", admin.site.urls),
+    path("api/", include("ingestion.urls")),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
