3db017bdd31998c4c1bd73204c64e9ab9af182e5
chimp-20 <chimpuser281@gmail.com>
2026-05-28 18:31:08 +0530

    change of vite to react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,7 +34,7 @@ function App() {
   const [themeGlow, setThemeGlow] = useState(true);
 
 
-  const API_BASE = import.meta.env.VITE_API_BASE;
+  const API_BASE = process.env.REACT_APP_API_BASE;
 
 
   const fetchSummary = async () => {
